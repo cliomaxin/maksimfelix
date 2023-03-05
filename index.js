a0d20e9823e45eb328aa3358a48e3cdd7a8fc9bf
developermaxwellkaranja <developermaxwellkaranja@gmail.com>
2023-03-05 21:18:42 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+var sidebar = document.getElementById("sidebar");
+   function showMenu(){
+    sidebar.style.right = "0";
+   }
+   
+   function hideMenu(){
+    sidebar.style.right = "40rem";
+   }
+   
+
+
+var typed = new Typed(".input", {
+strings:["Maxwell Karanja.",  "a Software Engineer.", "a Web developer.", "Tech and IT enthusiast"],
+typeSpeed: 150,
+backSpeed: 40,
+loop: true,
+})
+
