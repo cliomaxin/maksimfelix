137f2ed1c4316aa49180de3651179224c8309b04
developermaxwellkaranja <developermaxwellkaranja@gmail.com>
2023-05-25 21:14:11 +0300

    final essential touches

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var sidebar = document.getElementById("sidebar");
 
 
 var typed = new Typed(".input", {
-strings:["Maxwell Karanja.",  "a Full Stack Software Engineer.", "a Web developer.", "Content Writer & blogger", "IT enthusiast"],
+strings:["Maxwell Karanja.",  "a Full Stack Software Engineer.", "a Web developer.", "a Content Writer & blogger", "an IT enthusiast"],
 typeSpeed: 150,
 backSpeed: 15,
 loop: true,
